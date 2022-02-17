ad34dd59fa8b4ac506d54aa445a0687a07b446c5
Achtuur <arthurdegroot88@gmail.com>
2022-02-17 17:29:15 +0100

    added pages

--- AntennaAss.py
+++ AntennaAss.py
@@ -2,3 +2,15 @@ from SLiCAP import *
 
 # Initialize a project
 prj = initProject('Active antenna assignment')
+
+htmlPage('Environmental factors')
+file2html('environmental.txt')
+
+htmlPage('Cost factors')
+file2html('cost.txt')
+
+htmlPage('Functional factors')
+file2html('functional.txt')
+
+htmlPage('Performance factors')
+file2html('performance.txt')
